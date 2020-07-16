9831297f49e9c89c5cb5c54e1aba4b6a1d80b468
kachergis <george.kachergis@gmail.com>
2020-07-15 23:27:01 -0700

    fixed path

--- runAPI.R
+++ runAPI.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(plumber)
-pr <- plumb('plumber.R')
+pr <- plumb('cdi-cat/plumber.R')
 pr$run(port=4000, host="0.0.0.0")
 # Setting the host option on a VM instance ensures the application can be accessed externally.
 # (This may be only true for Linux users.)
