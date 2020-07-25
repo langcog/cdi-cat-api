ace033dd438249060ae5d87e99f01284f0537a34
Spencer <spencermehta@gmail.com>
2020-07-25 23:28:19 +0100

    unboxed startItem, nextItem

--- app/API/plumber.R
+++ app/API/plumber.R
@@ -15,17 +15,19 @@ function(){
 
 #* Get start item index and definition
 #* @param age_mos Child's age in months (12-36)
+#* @serializer unboxedJSON
 #* @get /startItem
 get_start_item <- function(age_mos) {
   if(age_mos<12 | age_mos>36) start_it = 'MI' # or indicate out of age range
   start_it = subset(age_startits, age==age_mos)
-  return(list(index = start_it$index,
-              definition = start_it$definition))
+  return(list(index = start_it$index[1],
+              definition = start_it$definition[1]))
 }
 
 #* Get next CAT item
 #* @param items Sequence of item IDs presented so far to a user (e.g.: [35,10])
 #* @param responses User's responses (0/1) to each item (e.g., [1,0])
+#* @serializer unboxedJSON
 #* @get /nextItem
 processCAT <- function(items, responses) {
   items = unlist(fromJSON(items))
@@ -40,9 +42,9 @@ processCAT <- function(items, responses) {
   }
   print(catd$person$thetas_history)
   nextItem = findNextItem(catd)
-  return(list(itemIndex=nextItem, 
-              itemDefinition=questions[nextItem], 
-              curTheta=catd$person$thetas))
+  return(list(index=nextItem, 
+              definition=questions[nextItem], 
+              curTheta=catd$person$thetas[1]))
 }
 
 
