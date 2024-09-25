bf4cfb859c7d3639add8b58e0fb1618205925adb
henrymehta <hjsmehta@gmail.com>
2024-09-25 10:37:58 +0100

    Removing test scripts from main program

--- app/API/runCAT.R
+++ app/API/runCAT.R
@@ -83,86 +83,86 @@ estimate_vocab_size <- function(theta, language, nsim=1000) {
   return(list(mean=mean(rowSums(p)), 
               sd=sd(rowSums(p))))
 }
-
-# test sequence generated given age, language, and theta (standardized language ability)
-# input response (0="not produces" / 1="produces"), update theta, get new item
-test <- function(age=24, language="EN", theta=1) {
-  # get response full CDI response pattern for given theta
-  responses <- generate_pattern(irt_models[[language]], Theta = theta)
-  # initialize CAT for given age and language
-  catd = initializeCAT(age, language)
-  nextItem = findNextItem(catd) # the start item
-  print(nextItem)
-  #responses = rep(c(0,1), 30) # generated alternating responses
-  while(!catd$design@stop_now) {
-    catd <- updateCAT(catd, nextItem, responses[nextItem])
-    nextItem = findNextItem(catd)
-  }
-  
-  print(get_CAT_summary(catd, language))
-}
-
-
-# generate
-#en24 <- test(24, "EN", theta=1)
-
-#sp12 <- test(12, "SP", theta=1)
-
-#fr18 <- test(18, "FR", theta=1)
-
-#jp18 <- test(18, "JP", theta=1)
-
-#jp24 <- test(24, "JP", theta=1)
-
-# test against a given sequence
-test_given_sequence <- function(age=24, language="EN", resp_seq) {
-  # initialize CAT for given age and language
-  catd = initializeCAT(age, language)
-  nextItem = findNextItem(catd) # the start item
-  print(nextItem)
-  #responses = rep(c(0,1), 30) # generated alternating responses
-  for(i in 1:nrow(resp_seq)) {
-    if(!catd$design@stop_now) {
-      # compare given and found items:
-      given_item_num = which(irt_coefs[[language]]$definition==resp_seq$item[i])
-      catd <- updateCAT(catd, nextItem, resp_seq[i,]$response)
-      nextItem = findNextItem(catd)
-    }
-  } 
-  
-  print(get_CAT_summary(catd, language))
-}
-
-# English
-en1 <- read.csv("test_sequences/en_CAT_test_theta0_ball.csv") # 14
-en2 <- read.csv("test_sequences/en_CAT_test_theta1_leg.csv") # 25
-en1_test <- test_given_sequence(age=14, language="EN", en1)
-testthat::expect_identical(en1$item, en1_test$items) 
-en2_test <- test_given_sequence(age=25, language="EN", en2)
-testthat::expect_identical(en2$item, en2_test$items) 
-
-# Spanish
-sp1 <- read.csv("test_sequences/sp_CAT_test_theta0_agua.csv") # 12
-sp2 <- read.csv("test_sequences/sp_CAT_test_theta1_cama.csv") # 22
-sp1_test <- test_given_sequence(12, language="SP", sp1)
-testthat::expect_identical(sp1$item, sp1_test$items) 
-sp2_test <- test_given_sequence(22, language="SP", sp2)
-testthat::expect_identical(sp2$item, sp2_test$items) 
-
-# French
-fr1 <- read.csv("test_sequences/fr_CAT_test_theta0_12mos.csv")
-fr2 <- read.csv("test_sequences/fr_CAT_test_theta1_24mos.csv")
-fr1_test <- test_given_sequence(12, language="FR", fr1)
-testthat::expect_identical(fr1$item, fr1_test$items) 
-fr2_test <- test_given_sequence(24, language="FR", fr2)
-testthat::expect_identical(fr2$item, fr2_test$items) 
-
-# Japanese
-jp1 <- read.csv("test_sequences/jp_CAT_test_theta0_12mos.csv") %>%
-  rename(item_id = item) %>% left_join(irt_coefs$JP %>% select(item_id, definition))
-jp2 <- read.csv("test_sequences/jp_CAT_test_theta1_24mos.csv") %>%
-  rename(item_id = item) %>% left_join(irt_coefs$JP %>% select(item_id, definition))
-jp1_test <- test_given_sequence(12, language="JP", jp1)
-testthat::expect_identical(jp1$definition, jp1_test$items) 
-jp2_test <- test_given_sequence(24, language="JP", jp2)
-testthat::expect_identical(jp2$definition, jp2_test$items) 
+# 
+# # test sequence generated given age, language, and theta (standardized language ability)
+# # input response (0="not produces" / 1="produces"), update theta, get new item
+# test <- function(age=24, language="EN", theta=1) {
+#   # get response full CDI response pattern for given theta
+#   responses <- generate_pattern(irt_models[[language]], Theta = theta)
+#   # initialize CAT for given age and language
+#   catd = initializeCAT(age, language)
+#   nextItem = findNextItem(catd) # the start item
+#   print(nextItem)
+#   #responses = rep(c(0,1), 30) # generated alternating responses
+#   while(!catd$design@stop_now) {
+#     catd <- updateCAT(catd, nextItem, responses[nextItem])
+#     nextItem = findNextItem(catd)
+#   }
+#   
+#   print(get_CAT_summary(catd, language))
+# }
+# 
+# 
+# # generate
+# #en24 <- test(24, "EN", theta=1)
+# 
+# #sp12 <- test(12, "SP", theta=1)
+# 
+# #fr18 <- test(18, "FR", theta=1)
+# 
+# #jp18 <- test(18, "JP", theta=1)
+# 
+# #jp24 <- test(24, "JP", theta=1)
+# 
+# # test against a given sequence
+# test_given_sequence <- function(age=24, language="EN", resp_seq) {
+#   # initialize CAT for given age and language
+#   catd = initializeCAT(age, language)
+#   nextItem = findNextItem(catd) # the start item
+#   print(nextItem)
+#   #responses = rep(c(0,1), 30) # generated alternating responses
+#   for(i in 1:nrow(resp_seq)) {
+#     if(!catd$design@stop_now) {
+#       # compare given and found items:
+#       given_item_num = which(irt_coefs[[language]]$definition==resp_seq$item[i])
+#       catd <- updateCAT(catd, nextItem, resp_seq[i,]$response)
+#       nextItem = findNextItem(catd)
+#     }
+#   } 
+#   
+#   print(get_CAT_summary(catd, language))
+# }
+# 
+# # English
+# en1 <- read.csv("test_sequences/en_CAT_test_theta0_ball.csv") # 14
+# en2 <- read.csv("test_sequences/en_CAT_test_theta1_leg.csv") # 25
+# en1_test <- test_given_sequence(age=14, language="EN", en1)
+# testthat::expect_identical(en1$item, en1_test$items) 
+# en2_test <- test_given_sequence(age=25, language="EN", en2)
+# testthat::expect_identical(en2$item, en2_test$items) 
+# 
+# # Spanish
+# sp1 <- read.csv("test_sequences/sp_CAT_test_theta0_agua.csv") # 12
+# sp2 <- read.csv("test_sequences/sp_CAT_test_theta1_cama.csv") # 22
+# sp1_test <- test_given_sequence(12, language="SP", sp1)
+# testthat::expect_identical(sp1$item, sp1_test$items) 
+# sp2_test <- test_given_sequence(22, language="SP", sp2)
+# testthat::expect_identical(sp2$item, sp2_test$items) 
+# 
+# # French
+# fr1 <- read.csv("test_sequences/fr_CAT_test_theta0_12mos.csv")
+# fr2 <- read.csv("test_sequences/fr_CAT_test_theta1_24mos.csv")
+# fr1_test <- test_given_sequence(12, language="FR", fr1)
+# testthat::expect_identical(fr1$item, fr1_test$items) 
+# fr2_test <- test_given_sequence(24, language="FR", fr2)
+# testthat::expect_identical(fr2$item, fr2_test$items) 
+# 
+# # Japanese
+# jp1 <- read.csv("test_sequences/jp_CAT_test_theta0_12mos.csv") %>%
+#   rename(item_id = item) %>% left_join(irt_coefs$JP %>% select(item_id, definition))
+# jp2 <- read.csv("test_sequences/jp_CAT_test_theta1_24mos.csv") %>%
+#   rename(item_id = item) %>% left_join(irt_coefs$JP %>% select(item_id, definition))
+# jp1_test <- test_given_sequence(12, language="JP", jp1)
+# testthat::expect_identical(jp1$definition, jp1_test$items) 
+# jp2_test <- test_given_sequence(24, language="JP", jp2)
+# testthat::expect_identical(jp2$definition, jp2_test$items) 
